32c8fe36abc25e03b572288d28a5289e9a156e46
Inafer <refanudin2106@gmail.com>
2023-10-19 11:22:29 +0700

    refan update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [initialValue, setInitialValue] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -17,8 +18,20 @@ function App() {
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+
+      <div className="input-container">
+        <label>Initial Value:</label>
+        <input
+          type="number"
+          value={initialValue}
+          onChange={(e) => setInitialValue(parseInt(e.target.value, 10))}
+        />
+        <button onClick={() => setCount(initialValue)}>Set Count</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+import './App.css';
+
